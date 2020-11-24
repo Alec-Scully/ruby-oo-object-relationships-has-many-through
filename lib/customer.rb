5fbef2f1aa58d8cf0ffb33dd94dcf796bd83ee56
Alec Scully <ScullyAlec@gmail.com>
2020-11-24 15:02:15 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Customer
-  
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        new_meal = Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        my_meals = Meal.all.select do | meals |
+            meals.customer == self
+        end
+        my_meals
+    end
+
+    def waiters
+        my_waiters = self.meals.map do | my_meals |
+            my_meals.waiter
+        end
+        my_waiters
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
+    attr_accessor :name, :years_experience
 
+    @@all = []
+
+    def initialize(name, years_experience)
+        @name = name
+        @years_experience = years_experience
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        new_meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        waiter_meals = Meal.all.select do | meals |
+            meals.waiter == self
+        end
+    end
+
+    def best_tipper
+        best_tip = 0
+        best_tipper = nil
+        meals.select do | meals|
+            if meals.tip > best_tip
+                best_tipper = meals.customer
+                best_tip = meals.tip
+            end
+            # binding.pry
+        end
+        best_tipper
+    end
 end
